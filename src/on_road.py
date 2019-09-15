16f12e79a062a15c451dd3de9e9da7ecc9e5cf01
dabao12321 <amli7@comcast.net>
2019-09-15 01:08:39 -0400

    function for location outliers wrt road width

--- src/on_road.py
+++ src/on_road.py
@@ -24,17 +24,38 @@ def in_polygon(lat, lon, shp, transform=False):
         pt = (lat,lon)
 
     val = False
+
     for i in range(len(all_shapes)):
         boundary = all_shapes[i] # get a boundary polygon
         if Point(pt).within(shape(boundary)): # make a point and see if it's in the polygon
-           name = all_records[i][2] # get the second field of the corresponding record
+           name = all_records[i]["name"] # get the second field of the corresponding record
            # print("The point is in", name)
            val = True
     return val
 
+def outofwidth(points, shp):
+    #point = [lat, long, time]
+    mintime = points[0][2]
+    maxtime = 0
+    out = 0
+    for point in points:
+        if point[2] > maxtime:
+            maxtime = point[2]
+        if point[2] < mintime:
+            mintime = point[2]
+        if in_polygon(point[0], point[1], shp):
+            out += 1
+    period = maxtime - mintime
+    return out/len(points), out/period
+
+
+
+
 if __name__ == "__main__":
     myfile = load_roads("../data/testroads")
     print(myfile)
+    for road in myfile:
+        print(road.shape.points)
 
     poly = load_roads("../data/polytest")
     print(in_polygon(-71.0935, 42.3593, poly))
